4bc4d5847361468208dc7886d66c82a460d5179e
Cassini-17 <Alex.tsybulnikov@gmail.com>
2023-12-08 15:25:30 +0000

    add form

--- things/views.py
+++ things/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from things.forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a new URL or show a success message
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
